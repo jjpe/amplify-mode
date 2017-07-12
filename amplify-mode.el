c16af94e4d4456fe1a522b60b32f33dc4bed4d03
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-07-12 11:00:50 +0200

    Bump version to 0.15.1

--- amplify-mode.el
+++ amplify-mode.el
@@ -72,7 +72,7 @@ explicitly included."
 
 
 
-(defvar amplify/semver "0.15.0")
+(defvar amplify/semver "0.15.1")
 
 ;; Download and load the `amplify' core:
 (defvar amplify/amplify-root-dir (amplify/subproc-path "amplify"))
