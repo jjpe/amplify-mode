20fd34b67d4fa971730eb795c26a47f53724e672
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-07-12 13:45:32 +0200

    Upgrade dependencies

--- amplify-mode.el
+++ amplify-mode.el
@@ -82,7 +82,7 @@ explicitly included."
 
 ;; Download and load `amplify-elisp':
 (defvar amplify/amplify-elisp-root-dir (amplify/subproc-path "amplify-elisp"))
-(defvar amplify/amplify-elisp-version "0.15.1")
+(defvar amplify/amplify-elisp-version "0.15.2")
 (defvar amplify/amplify-elisp-current-dir
   (concat amplify/amplify-elisp-root-dir "/" amplify/amplify-elisp-version))
 
--- depend.el/depend.el
+++ depend.el/depend.el
@@ -4,9 +4,15 @@
 
 ;;; Code:
 
-(require 'dash) ;; threading macros
+(unless (package-installed-p 'dash)
+  (package-install 'dash))
+(unless (package-installed-p 'json)
+  (package-install 'json))
 
-(defvar depend/semver "0.5.3"
+(require 'dash) ;; threading macros
+(require 'json)
+
+(defvar depend/semver "0.5.4"
   "The semantic version of this depend.el release.")
 
 (defvar depend/bin-semver "0.1.0"
