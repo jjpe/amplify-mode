60ac09631133fad1e2e152a313ebbf82539b7173
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-07-14 18:58:51 +0200

    Upgrade to amplify 0.14.1

--- amplify-mode.el
+++ amplify-mode.el
@@ -86,7 +86,7 @@ explicitly included."
 
 ;; Download and load the `amplify' core:
 (defvar amplify/amplify-root-dir (amplify/subproc-path "amplify"))
-(defvar amplify/amplify-version "0.14.0")
+(defvar amplify/amplify-version "0.14.1")
 (defvar amplify/amplify-current-dir
   (concat amplify/amplify-root-dir "/" amplify/amplify-version))
 
