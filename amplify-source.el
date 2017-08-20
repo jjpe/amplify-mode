62decdb24074c27a7760dc061a81804577c7a2e7
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-08-20 03:18:27 +0200

    amplify/report: Add request-number key

--- amplify-source.el
+++ amplify-source.el
@@ -174,7 +174,7 @@ AST: either nil (default) or a plistified AST, see `amplify-elisp/ast-plistify'.
                                      msg))
           ((eq result nil)         msg))))
 
-(cl-defun amplify/report (&key kind contents)
+(cl-defun amplify/report (&key request-number kind contents)
   "Make the Source send a report message.  The following keys are significant:
 KIND: What the Msg represents, e.g. 'parse', 'analyze', 'analysis result',
     'syntax coloring' etc.
@@ -182,7 +182,7 @@ CONTENTS: The Msg contents.  May be nil, a string, or a list of strings."
   (unless amplify/source
     (error "Source is not connected"))
   (let* ((process "emacs")
-         (request-number (amplify/next-request-number))
+         (request-number (or request-number (amplify/next-request-number)))
          (msg (amplify-elisp/msg :process process
                                  :request-number request-number
                                  :kind kind
